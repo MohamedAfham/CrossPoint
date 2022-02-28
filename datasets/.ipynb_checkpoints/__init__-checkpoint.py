c5f2223205c8e993fe8ac8e0f9f6c79f588dee63
MohamedAfham <afhamaflal9@gmail.com>
2022-02-28 17:51:50 +0000

    Initial Commit

--- datasets/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from . import data
-from . import plyfile
-from . import data_utils
-from . import shapenet_part
--- scripts/.ipynb_checkpoints/fsl_script-checkpoint.sh
+++
@@ -1,1 +0,0 @@
-python eval_fewshot.py --model_path path-to-trained-model --dataset modelnet40 --k_way 5 --m_shot 10 --n_runs 50
