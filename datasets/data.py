4262f6ff03a770c8e972b0a524e41dfd42fbcfe9
auniquesun <Mr.sunhy@outlook.com>
2022-04-05 21:52:35 +0800

    update data dir and relatives

--- datasets/data.py
+++ datasets/data.py
@@ -40,8 +40,8 @@ trans_2 = transforms.Compose(
             ])
 
 def load_modelnet_data(partition):
-    BASE_DIR = ''
-    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    DATA_DIR = '/mnt/sdb/public/data/common-datasets'
+    # DATA_DIR = os.path.join(BASE_DIR, 'data')
     all_data = []
     all_label = []
     for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
@@ -56,7 +56,7 @@ def load_modelnet_data(partition):
     return all_data, all_label
 
 def load_ScanObjectNN(partition):
-    BASE_DIR = 'data/ScanObjectNN'
+    BASE_DIR = '/mnt/sdb/public/data/common-datasets/ScanObjectNN'
     DATA_DIR = os.path.join(BASE_DIR, 'main_split')
     h5_name = os.path.join(DATA_DIR, f'{partition}.h5')
     f = h5py.File(h5_name)
@@ -66,24 +66,35 @@ def load_ScanObjectNN(partition):
     return data, label
 
 def load_shapenet_data():
-    BASE_DIR = ''
-    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    DATA_DIR = '/mnt/sdb/public/data/common-datasets'
+    # DATA_DIR = os.path.join(BASE_DIR, 'data')
     all_filepath = []
 
+    # print('-'*5, 'IN load_shapenet_data()')
     for cls in glob.glob(os.path.join(DATA_DIR, 'ShapeNet/*')):
         pcs = glob.glob(os.path.join(cls, '*'))
+        # print('-'*5, pcs)
         all_filepath += pcs
         
+    # print('all_filepath', all_filepath)
     return all_filepath
 
 def get_render_imgs(pcd_path):
+    # print('-'*5, 'IN get_render_imgs()')
+
+    # print('-'*5, pcd_path)
     path_lst = pcd_path.split('/')
-    path_lst[1] = 'ShapeNetRendering'
+
+    # path_lst[1] = 'ShapeNetRendering'
+    path_lst[-3] = 'ShapeNetRendering'  # changed by jerry, because the directory of download datasets is different
     path_lst[-1] = path_lst[-1][:-4]
+
     path_lst.append('rendering')
     
     DIR = '/'.join(path_lst)
     img_path_list = glob.glob(os.path.join(DIR, '*.png'))
+
+    # print('-'*5, img_path_list)
     
     return img_path_list
         
